70ea0ced59c198a7de80210846a8f7d68cffb943
soar <1147358383@qq.com>
2025-07-13 19:58:20 +0800

    本地调用MCP服务（高德地图MCP）

--- src/test/java/com/fei/feiaiagent/app/LoveAppTest.java
+++ src/test/java/com/fei/feiaiagent/app/LoveAppTest.java
@@ -75,4 +75,13 @@ class LoveAppTest {
         String answer = loveApp.doChatWithTools(message, chatId);
         Assertions.assertNotNull(answer);
     }
+
+    @Test
+    void doChatWithMcp() {
+        String chatId = UUID.randomUUID().toString();
+        // 测试地图 MCP
+        String message = "我想和另一半去上海静安区转转，请帮我找到5公里内合适的约会地点";
+        String answer = loveApp.doChatWithMcp(message, chatId);
+        Assertions.assertNotNull(answer);
+    }
 }
